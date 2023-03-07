0452da50eba341899a7c0b367640b103c047ea7a
LHensman <lukehensman7@gmail.com>
2023-03-07 12:40:34 +0000

    front end changes to remote back end

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -2,7 +2,7 @@ angular.module('app').factory('ConfigService', [function() {
 
     var service = {};
 
-    service.apiBase = 'http://localhost:8001/';
+    service.apiBase = 'https://quizapilh-env.eba-wpuv9dpm.eu-west-2.elasticbeanstalk.com/';
 
     return service;
 }]);
